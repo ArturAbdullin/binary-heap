b3d59f89fc85d35c30406913d538b4e99af17cec
Artur Abdullin <artur.abdullin@gmail.com>
2022-09-19 19:19:58 +0300

    feat: add a pop() method

--- MinHeap.js
+++ MinHeap.js
@@ -72,4 +72,79 @@ class MinHeap {
       [childIdx, parentIdx] = [parentIdx, Math.floor(childIdx / 2)];
     }
   }
+
+  /**
+   * Removes the min heap element and returns it. If the heap is empty returns `undefined` and the heap is not modified.
+   * @returns {number | undefined} min element
+   */
+  pop() {
+    if (this.#currSize <= 0) return undefined;
+    const minVal = this.#heap[1];
+
+    if (this.#currSize === 1) {
+      this.#heap[1] = undefined;
+      this.#currSize--;
+      return minVal;
+    }
+
+    if (this.#currSize === 2) {
+      [this.#heap[1], this.#heap[2]] = [this.#heap[2], undefined];
+      this.#currSize--;
+      return minVal;
+    }
+
+    this.#heap[1] = this.#heap[this.#currSize];
+    this.#heap[this.#currSize] = undefined;
+    this.#currSize--;
+    this.#heapifyDown(1);
+    return minVal;
+  }
+
+  /**
+   * Traverse down the heap to find a suitable place for the number at index `idx`.
+   * @param {number} idx
+   */
+  #heapifyDown(idx) {
+    let parentIdx = idx;
+    let [leftChildIdx, rightChildIdx] = this.#leftRightFrom(parentIdx);
+
+    while (
+      this.#heap[parentIdx] > this.#heap[leftChildIdx] ||
+      this.#heap[parentIdx] > this.#heap[rightChildIdx]
+    ) {
+      if (this.#heap[rightChildIdx]) {
+        if (this.#heap[leftChildIdx] < this.#heap[rightChildIdx]) {
+          this.#swap(leftChildIdx, parentIdx);
+          parentIdx = leftChildIdx;
+        } else {
+          this.#swap(rightChildIdx, parentIdx);
+          parentIdx = rightChildIdx;
+        }
+      } else {
+        this.#swap(leftChildIdx, parentIdx);
+        parentIdx = leftChildIdx;
+      }
+      [leftChildIdx, rightChildIdx] = this.#leftRightFrom(parentIdx);
+    }
+  }
+
+  /**
+   * Swap heap elements at `idx1` and `idx2`
+   * @param {number} idx1
+   * @param {number} idx2
+   */
+  #swap(idx1, idx2) {
+    [this.#heap[idx1], this.#heap[idx2]] = [this.#heap[idx2], this.#heap[idx1]];
+  }
+
+  /**
+   * Return left and right children indices of a parent at `idx`
+   * @param {number} idx parent index.
+   * @returns {number[]} left and right children indices.
+   */
+  #leftRightFrom(idx) {
+    return [idx * 2, idx * 2 + 1];
+  }
 }
+
+module.exports = MinHeap;
